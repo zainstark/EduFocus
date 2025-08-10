c60c180bb72f45b48d6f1d65d53c8f1c2260a83f
zainstark <yousef.2520092@gmail.com>
2025-08-10 22:03:19 +0300

    Initial commit

--- back/back/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home_view, name='home'),
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='auth'),
-]
